b68327559a24fb07f9459a22334c3c33efb5b102
Krzysztof Romanowski <krzysztof.romanowski@zendesk.com>
2022-12-31 17:39:08 +0100

    Change tests to Unix format for easier read

--- src/lightController/lightController_test.go
+++ src/lightController/lightController_test.go
@@ -8,120 +8,112 @@ import (
 	sunAPI "raspberrypi.local/triggersApi/sunPositionAPI"
 )
 
-// Important: Dates are in UTC
-// So noon in life is 2022-12-04T11:00:00 in UTC
 var sunPositionAPI = sunAPI.SunPosition{
-	Sunrise: getParsedDate("2022-12-04T06:25:13"),
-	Sunset:  getParsedDate("2022-12-04T14:27:25"),
+	Sunrise: getParsedDate("Sun Dec 4 07:25:13 CET 2022"),
+	Sunset:  getParsedDate("Sun Dec 4 15:27:25 CET 2022"),
 }
 
 var location = getLocation()
 var sut = light.NewLightController(sunPositionAPI)
 
+func TestTriggerAtNight(t *testing.T) {
+	if getTriggerKey(getParsedDate("Sat Dec 3 23:59:00 CET 2022")) != "trigger-1" {
+		t.Fail()
+	}
+	if getTriggerKey(getParsedDate("Sun Dec 4 02:59:00 CET 2022")) != "trigger-1" {
+		t.Fail()
+	}
+	if getTriggerKey(getParsedDate("Sun Dec 4 01:00:00 CET 2022")) != "trigger-1" {
+		t.Fail()
+	}
+	if getTriggerKey(getParsedDate("Sun Dec 4 00:00:00 CET 2022")) != "trigger-1" {
+		t.Fail()
+	}
+	if getTriggerKey(getParsedDate("Sun Dec 4 23:01:00 CET 2022")) != "trigger-1" {
+		t.Fail()
+	}
+	if getTriggerKey(getParsedDate("Sun Dec 4 23:59:00 CET 2022")) != "trigger-1" {
+		t.Fail()
+	}
+	if getTriggerKey(getParsedDate("Mon Dec 5 00:01:00 CET 2022")) != "trigger-1" {
+		t.Fail()
+	}
+}
+
 func TestTriggerAtNoon(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-04T10:25:00")) != "trigger-5" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 11:00:00 CET 2022")) != "trigger-5" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T10:00:00")) != "trigger-5" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 12:59:00 CET 2022")) != "trigger-5" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T11:00:00")) != "trigger-5" {
-		t.Fail()
-	}
-	if getTriggerKey(getParsedDate("2022-12-04T11:00:00")) != "trigger-5" {
-		t.Fail()
-	}
-	if getTriggerKey(getParsedDate("2022-12-04T12:00:00")) != "trigger-5" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 13:00:00 CET 2022")) != "trigger-5" {
 		t.Fail()
 	}
 }
 
 func TestTriggerRightBeforeAndAfterNoon(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-04T07:30:00")) != "trigger-4" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 08:30:00 CET 2022")) != "trigger-4" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T09:59:59")) != "trigger-4" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 10:59:59 CET 2022")) != "trigger-4" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T12:00:01")) != "trigger-4" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 13:00:01 CET 2022")) != "trigger-4" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T13:27:01")) != "trigger-4" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 14:27:01 CET 2022")) != "trigger-4" {
 		t.Fail()
 	}
 }
 
 func TestTriggerSunriseToSunrisePlusOne(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-04T06:26:00")) != "trigger-3" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 07:26:00 CET 2022")) != "trigger-3" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T07:25:00")) != "trigger-3" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 08:25:00 CET 2022")) != "trigger-3" {
 		t.Fail()
 	}
 }
 
 func TestTriggerSunsetMinusOneToSunset(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-04T13:28:00")) != "trigger-3" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 14:28:00 CET 2022")) != "trigger-3" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T14:27:00")) != "trigger-3" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 15:27:00 CET 2022")) != "trigger-3" {
 		t.Fail()
 	}
 }
 
 func TestTriggerEarlyMorning(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-04T02:01:00")) != "trigger-2" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 03:01:00 CET 2022")) != "trigger-2" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T06:25:00")) != "trigger-2" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 07:25:00 CET 2022")) != "trigger-2" {
 		t.Fail()
 	}
 }
 
 func TestTriggerEvening(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-04T14:28:00")) != "trigger-6" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 15:28:00 CET 2022")) != "trigger-6" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T21:00:00")) != "trigger-6" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 22:00:00 CET 2022")) != "trigger-6" {
 		t.Fail()
 	}
 }
 
 func TestTriggerLateEvening(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-04T21:00:01")) != "trigger-7" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 22:00:01 CET 2022")) != "trigger-7" {
 		t.Fail()
 	}
-	if getTriggerKey(getParsedDate("2022-12-04T22:00:00")) != "trigger-7" {
-		t.Fail()
-	}
-}
-
-func TestTriggerNightSameDay(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-04T23:00:01")) != "trigger-1" {
-		t.Fail()
-	}
-	if getTriggerKey(getParsedDate("2022-12-04T00:00:00")) != "trigger-1" {
-		t.Fail()
-	}
-	if getTriggerKey(getParsedDate("2022-12-04T01:00:01")) != "trigger-1" {
-		t.Fail()
-	}
-	if getTriggerKey(getParsedDate("2022-12-04T22:00:01")) != "trigger-1" {
-		t.Fail()
-	}
-}
-
-func TestTriggerNightTheNextDay(t *testing.T) {
-	if getTriggerKey(getParsedDate("2022-12-05T23:00:01")) != "trigger-1" {
-		t.Fail()
-	}
-	if getTriggerKey(getParsedDate("2022-12-05T00:00:01")) != "trigger-1" {
+	if getTriggerKey(getParsedDate("Sun Dec 4 23:00:00 CET 2022")) != "trigger-7" {
 		t.Fail()
 	}
 }
 
 func getParsedDate(date string) time.Time {
-	d, _ := time.Parse("2006-01-02T15:04:05", date)
+	d, _ := time.Parse(time.UnixDate, date)
 	return d
 }
 
